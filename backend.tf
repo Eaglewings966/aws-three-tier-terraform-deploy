899af6cd79cf4de0c71f74cde24eb1118dbf5ca4
ubaniobiajulumemmanuel <eaglewynx@gmail.com>
2026-01-10 21:02:39 +0100

    chore: sanitize repo ownership, replace branding, secure defaults

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "digitalwitchngbucketcloud1"
-    key    = "digitalwitchng/prodution/terraform.tfstate"
+    bucket = "NovaTrust Bankngbucketcloud1"
+    key    = "NovaTrust Bankng/prodution/terraform.tfstate"
     region = "us-west-1"
   }
 }
